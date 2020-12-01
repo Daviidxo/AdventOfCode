a4317bf1615f79faa903f053d05e6404e61ec171
David Olah <david.olah999@gmail.com>
2020-12-01 18:24:08 +0100

    Add 2020 day 01

---
+++ src/Year2020/Day01Solution.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Daviidxo\AdventOfCode\Year2020;
+
+use Daviidxo\AdventOfCode\SolutionBase;
+
+class Day01Solution extends SolutionBase
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSolution(string $input): array
+    {
+        $data = file($input, FILE_IGNORE_NEW_LINES);
+
+        return [
+            'taskA' => $this->getTaskA($data),
+            'taskB' => $this->getTaskB($data),
+        ];
+    }
+
+    public function getTaskA(array $input): int
+    {
+        for ($i = 0; $i < count($input) - 1; $i++) {
+            for ($j = 1; $j < count($input); $j++) {
+                if ($input[$i] + $input[$j] !== 2020) {
+                    continue;
+                }
+
+                return $input[$i] * $input[$j];
+            }
+        }
+
+        return 0;
+    }
+
+    public function getTaskB(array $input): int
+    {
+        for ($i = 0; $i < count($input) - 2; $i++) {
+            for ($j = 1; $j < count($input) - 1; $j++) {
+                for ($k = 2; $k < count($input); $k++) {
+                    if ($input[$i] + $input[$j] + $input[$k] !== 2020) {
+                        continue;
+                    }
+
+                    return $input[$i] * $input[$j]  * $input[$k];
+                }
+            }
+        }
+
+        return 0;
+    }
+}
