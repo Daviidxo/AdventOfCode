5b14d6c84a0c29651ca9b7fa5e113e5797c075d7
David Olah <david.olah999@gmail.com>
2020-12-19 22:19:24 +0100

    Add 2020 day 09

---
+++ src/Year2020/Day09Solution.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Daviidxo\AdventOfCode\Year2020;
+
+use Daviidxo\AdventOfCode\SolutionBase;
+
+class Day09Solution extends SolutionBase
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSolution(string $input): array
+    {
+        $data = file($input, FILE_IGNORE_NEW_LINES);
+
+        return [
+            'taskA' => $this->getTaskA($data),
+            'taskB' => $this->getTaskB($data),
+        ];
+    }
+
+    public function getTaskA(array $data): int
+    {
+        $preambleLength = 25;
+
+        return $this->getFirstInvalid($data, $preambleLength);
+    }
+
+    public function getFirstInvalid(array $data, int $preambleLength): ?int
+    {
+        for ($i = $preambleLength; $i < count($data); $i++) {
+            $numbersBefore = array_slice($data, $i - $preambleLength, $preambleLength);
+            if (!$this->isNumberValid((int) $data[$i], $numbersBefore)) {
+                return (int) $data[$i];
+            }
+        }
+
+        return null;
+    }
+
+    public function isNumberValid(int $number, array $numbersBefore): bool
+    {
+        for ($i = 0; $i < count($numbersBefore) - 1; $i++) {
+            for ($j = 1; $j < count($numbersBefore); $j++) {
+                if ($numbersBefore[$i] + $numbersBefore[$j] === $number) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public function getTaskB(array $data): int
+    {
+        $firstInvalid = $this->getTaskA($data);
+
+        $i = 0;
+        $set = [];
+        $currentSum = 0;
+
+        while ($currentSum !== $firstInvalid) {
+            $currentSum += $data[$i];
+            $set[] = $data[$i];
+            for ($j = $i + 1; $j < count($data); $j++) {
+                $currentSum += $data[$j];
+
+                if ($currentSum === $firstInvalid) {
+                    $set[] = $data[$j];
+                    break;
+                }
+
+                if ($currentSum > $firstInvalid) {
+                    $set = [];
+                    $currentSum = 0;
+                    $i++;
+
+                    break;
+                }
+
+                $set[] = $data[$j];
+            }
+        }
+
+        return min($set) + max($set);
+    }
+}
